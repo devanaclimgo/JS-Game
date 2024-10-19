72fdd81f535d85279d3bbca3ed35e8afa3f5cba9
devanaclimgo <anaclimgo@gmail.com>
2024-10-19 19:17:12 -0300

    criada função verificar chute

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ titulo.innerHTML = 'Jogo do número secreto'
 
 let paragrafo = document.querySelector('p')
 paragrafo.innerHTML = 'Escolha um número de 1 a 10'
+
+const verificarChute = document.querySelector('.botao-chute')
+
+verificarChute.addEventListener('click', function() {
+  console.log('O botão foi clicado!')
+})
