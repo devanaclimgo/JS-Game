9ea65a367f72be673274d7a8b6b59a4e1498f6da
devanaclimgo <anaclimgo@gmail.com>
2024-10-22 18:40:34 -0300

    verificando o chute

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ const verificarChute = document.querySelector('.botao-chute')
 
 verificarChute.addEventListener('click', function() {
   let chute = document.querySelector('input').value
-  console.log(chute  == numeroSecreto)
+
+  if(chute === numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!')
+    exibirTextoNaTela('p', 'Você descobriu o número secreto!')
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela('p', 'O número secreto é menor')
+    } else {
+      exibirTextoNaTela('p', 'O número secreto é maior')
+    }
+  }
 })
 
 function gerarNumeroAleatorio() {
