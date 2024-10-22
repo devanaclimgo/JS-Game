f790fae6cce09c8cbd811ce407e8feeb7f90a8b7
devanaclimgo <anaclimgo@gmail.com>
2024-10-22 18:30:33 -0300

    função com retorno

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// let titulo = document.querySelector('h1')
-// titulo.innerHTML = 'Jogo do número secreto'
-
-// let paragrafo = document.querySelector('p')
-// paragrafo.innerHTML = 'Escolha um número de 1 a 10'
+let numeroSecreto = gerarNumeroAleatorio()
 
 function exibirTextoNaTela() {
   let campo = document.querySelector(tag)
@@ -15,5 +11,9 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 const verificarChute = document.querySelector('.botao-chute')
 
 verificarChute.addEventListener('click', function() {
-  console.log('O botão foi clicado!')
+  console.log(numeroSecreto)
 })
+
+function gerarNumeroAleatorio() {
+ return parseInt(Math.random() * 10 + 1)
+}
