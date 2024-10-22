329537aba2859737fc0cca365d36a4b81244e5ec
devanaclimgo <anaclimgo@gmail.com>
2024-10-22 19:19:36 -0300

    bug resolvido do chute == a numeroSecreto

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reiniciarJogo = document.querySelector('.reiniciar')
 verificarChute.addEventListener('click', function() {
   let chute = document.querySelector('input').value
 
-  if(chute === numeroSecreto) {
+  if(chute == numeroSecreto) {
     exibirTextoNaTela('h1', 'Acertou!')
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
     let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
